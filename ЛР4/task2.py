f958967d804829604792e476d3cf156514bf35b9
Sofia_Melashvili <sofiamelashvili@gmail.com>
2026-01-09 00:43:47 +0300

    Add files via upload

---
+++ ЛР4/task2.py
@@ -0,0 +1,26 @@
+# TODO импортировать необходимые молули
+import csv
+import json
+from typing import List, Dict
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+def task() -> None:
+    ...  # TODO считать содержимое csv файла
+    data: List[Dict[str, str]] = []
+    with open(INPUT_FILENAME, 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+    ...  # TODO Сериализовать в файл с отступами равными 4
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
